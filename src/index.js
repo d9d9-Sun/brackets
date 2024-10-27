27b2648571bc648001d4173699a1e1d70b592951
Dmitry Yarmakovich <d.yarmakovich@gmail.com>
2024-10-27 15:11:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let nStr = str;
+  for (let i = 0; i < bracketsConfig.length * 2; i++) {
+    for (let n = 0; n < bracketsConfig.length; n++) {
+      let sStr = bracketsConfig[n][0] + bracketsConfig[n][1];
+      for (let j = 0; j < str.length / 2; j++) {
+        nStr = nStr.replace(sStr, '');
+      }
+    }
+    if (nStr.length === 0) return true;
+  }
+  if (nStr.length > 0) return false;
 }
